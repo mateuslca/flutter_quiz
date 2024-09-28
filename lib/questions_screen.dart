f55ce8f005f7abc746c63095d43a1d6bc6b3d397
mateuslca <mateuslca2020@gmail.com>
2024-09-27 21:42:23 -0300

    add: QuestionsScreen

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -12,23 +12,28 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreen extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Text('The Question...'),
-        const SizedBox(height: 30),
-        ElevatedButton(
-          onPressed: () {},
-          child: const Text('Answer 1'),
-        ),
-        ElevatedButton(
-          onPressed: () {},
-          child: const Text('Answer 2'),
-        ),
-        ElevatedButton(
-          onPressed: () {},
-          child: const Text('Answer 3'),
-        ),
-      ],
+    //Need to wrap this in a SizedBox()
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('The Question...'),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 1'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 2'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 3'),
+          ),
+        ],
+      ),
     );
   }
 }
