a4107f61a3513cf506723517c08f3617d588473d
mateuslca <mateuslca2020@gmail.com>
2024-09-28 21:19:40 -0300

    add: styling on buttons

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quiz/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -20,17 +21,17 @@ class _QuestionsScreen extends State<QuestionsScreen> {
         children: [
           const Text('The Question...'),
           const SizedBox(height: 30),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Answer 1'),
+          AnswerButton(
+            answerText: 'answertext',
+            onTap: () {},
           ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Answer 2'),
+          AnswerButton(
+            answerText: 'answerText',
+            onTap: () {},
           ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Answer 3'),
+          AnswerButton(
+            answerText: 'answerText',
+            onTap: () {},
           ),
         ],
       ),
