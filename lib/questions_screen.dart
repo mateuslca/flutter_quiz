e89f50ff920b57dbc47a8f2b37c2fd9dd15d3781
mateuslca <mateuslca2020@gmail.com>
2024-10-02 22:19:27 -0300

    fix: mapping AnswerButtons now

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quiz/answer_button.dart';
+import 'package:flutter_quiz/data/questions_data.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -11,29 +12,37 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreen extends State<QuestionsScreen> {
+  final currentQuestion = questions[0]; //List of questions
+
   @override
   Widget build(BuildContext context) {
-    //Need to wrap this in a SizedBox()
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text('The Question...'),
-          const SizedBox(height: 30),
-          AnswerButton(
-            answerText: 'answertext',
-            onTap: () {},
-          ),
-          AnswerButton(
-            answerText: 'answerText',
-            onTap: () {},
-          ),
-          AnswerButton(
-            answerText: 'answerText',
-            onTap: () {},
-          ),
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+
+            // By using .map we can map every single ".answer" on the file to currentQuestion
+            // and use it to return as much AnswerButtons as needed.
+
+            ...currentQuestion.answers.map(
+              (answer) {
+                return AnswerButton(answerText: answer, onTap: () {});
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
